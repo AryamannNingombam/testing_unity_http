c4f08ac40cd18ec1c0b38502a97409b86572724d
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-09-28 17:49:39 +0530

    first

---
+++ Scripts/ClickListener.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ResponseFormat;
+
+public class ClickListener : MonoBehaviour
+{
+    const string requestURL = "http://localhost:8080/check-post";
+ 
+
+    public void ButtonClicked()
+    {
+        WWW test = new WWW(requestURL,returnPostBody(),returnWithHeaders());
+        StartCoroutine(SendPostRequest(test));
+        
+        
+    }
+    private IEnumerator SendPostRequest(WWW req)
+    {
+        yield return req;
+        string result = req.text;
+        PostResponse p = PostResponse.CreateFromJSON(result);
+        print(p.data.dateOfBirth.day);
+
+
+    }
+
+    private byte[] returnPostBody()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("email", "ningombam.singh@learner.manipal.edu");
+
+
+        return form.data;
+    }
+
+    private Dictionary<string,string> returnWithHeaders()
+    {
+        WWWForm form = new WWWForm();
+        Dictionary<string, string> d = form.headers;
+        d.Add("test-header", "header received boy");
+
+        return d;
+    }
+
+    private IEnumerator fetchHeaderRequest(WWW req)
+    {
+        yield return req;
+        string result = req.text;
+        HeaderResponse temp = HeaderResponse.CreateFromJSON(result);
+        print(temp.success);
+        print(temp.message);
+    }
+    private IEnumerator fetchWelcomeRequest(WWW req)
+    {
+        yield return req;
+        string result = req.text;
+        WelcomeResponse temp = WelcomeResponse.CreateFromJSON(result);
+        print(temp.message);
+
+
+    }
+
+}
---
+++ Scripts/ResponseFormat.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace ResponseFormat
+{
+    [System.Serializable]
+    public class WelcomeResponse
+    {
+        public string message;
+
+        public static WelcomeResponse CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<WelcomeResponse>(jsonString);
+
+        }
+
+    }
+
+    [System.Serializable]
+    public class HeaderResponse
+    {
+        public bool success;
+        public string message;
+        public static HeaderResponse CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<HeaderResponse>(jsonString);
+        }
+    }
+
+    [System.Serializable]
+    public class PostDateOfBirthFormat
+    {
+        public int year;
+        public int month;
+        public int day;
+    }
+
+    [System.Serializable]
+    public class PostDataFormat
+    {
+        public string username;
+        public string email;
+        public PostDateOfBirthFormat dateOfBirth;
+
+    }
+
+    [System.Serializable]
+    public class PostResponse
+    {
+        public bool success;
+        public PostDataFormat data;
+        public static PostResponse CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<PostResponse>(jsonString);
+        }
+    }
+
+
+}
